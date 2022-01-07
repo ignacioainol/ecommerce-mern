8aca1603a039d69d63aa4bb2e1bd0da76109ec34
Ignacio Ainol <iggnaxios@gmail.com>
2022-01-07 04:21:52 -0300

    create connection to mongodb local and atlas

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import path from 'path';
+
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('connected to db')
+}).catch(err => {
+    console.log(err.message)
+})
 
 const app = express();
 
