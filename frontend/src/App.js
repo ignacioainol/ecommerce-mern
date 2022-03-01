d6b8cf336e4dd02951132a6d381f27afa3b9b029
Ignacio Ainol <iggnaxios@gmail.com>
2022-03-01 19:25:19 -0300

    iterating each element of products and creating product info style css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 function App() {
   return (
@@ -7,7 +8,27 @@ function App() {
       </header>
 
       <main>
-        list products
+        <h1>Featured Products</h1>
+
+        <div className="products">
+          {
+            data.products.map(product => (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+
+                  <p> <strong> ${product.price}</strong> </p>
+                  <button>Add to Cart</button>
+                </div>
+              </div>
+            ))
+          }
+        </div>
       </main>
     </div>
   );
