eafa3deffd3a506c147f47d97a3b6a407d7f9582
Ignacio Ainol <iggnaxios@gmail.com>
2022-03-06 21:48:24 -0300

    test git date

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -25,6 +25,7 @@ export const HomeScreen = () => {
                         </div>
                     ))
                 }
+                {/* testing git */}
             </div>
         </div>
     )
