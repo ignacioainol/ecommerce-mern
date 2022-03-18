1c4a3952642dec7682d04be367492d13b1a61688
Ignacio Ainol <iggnaxios@gmail.com>
2022-03-18 03:56:21 -0300

    add fontawesome and rating component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useReducer, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useReducer } from 'react'
 import axios from 'axios';
 import logger from 'use-reducer-logger';
+import { Col, Row } from 'react-bootstrap';
+import Product from '../components/Product';
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -51,22 +52,15 @@ export const HomeScreen = () => {
                 {
                     loading ? (<div>Loading...</div>) :
                         error ? (<div>{error}</div>) :
-                            (products.map(product => (
-                                <div className="product" key={product.slug}>
-                                    <Link to={`/product/${product.slug}`}>
-                                        <img src={product.image} alt={product.name} />
-                                    </Link>
-                                    <div className="product-info">
-                                        <Link to={`/product/${product.slug}`}>
-                                            <p>{product.name}</p>
-                                        </Link>
-
-                                        <p> <strong> ${product.price}</strong> </p>
-                                        <button>Add to Cart</button>
-                                    </div>
-                                </div>
-                            )))
-                }
+                            (
+                                <Row>
+                                    {products.map(product => (
+                                        <Col key={product.slug} sm={6} md={4} lg={3}>
+                                            <Product product={product} />
+                                        </Col>
+                                    ))}
+                                </Row>
+                            )}
             </div>
         </div>
     )
