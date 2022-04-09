b8f1fe1611786e40985b28eca87f0976ed2c90e2
Ignacio Ainol <iggnaxios@gmail.com>
2022-04-09 15:01:03 -0400

    created sidebar and searchbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Navbar, Container, Nav, Badge, NavDropdown } from 'react-bootstrap';
+import { Navbar, Container, Nav, Badge, NavDropdown, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
@@ -15,6 +15,9 @@ import { PaymentMethodScreen } from './screens/PaymentMethodScreen';
 import { PlaceOrderScreen } from './screens/PlaceOrderScreen';
 import { OrderScreen } from './screens/OrderScreen';
 import OrderHistoryScreen from './screens/OrderHistoryScreen';
+import { ProfileScreen } from './screens/ProfileScreen';
+import axios from 'axios';
+import { SearchBox } from './components/SearchBox';
 
 function App() {
 
@@ -29,21 +32,43 @@ function App() {
     window.location.href = '/signin';
   }
 
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const { data } = await axios.get(`/api/products/categories`);
+      setCategories(data);
+    }
+    fetchCategories();
+  }, [])
+
+
   return (
     <BrowserRouter>
-      <ToastContainer position='bottom-center' limit={1} />
-      <div className='d-flex flex-column site-container'>
+      <div className={sidebarIsOpen
+        ? 'd-flex flex-column site-container active-cont'
+        : 'd-flex flex-column site-container'
+      }>
+        <ToastContainer position='bottom-center' limit={1} />
         <header>
           <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
+              <Button
+                variant="dark"
+                onClick={() => setSidebarIsOpen(!sidebarIsOpen)}
+              >
+                <i className="fas fa-bars"></i>
+              </Button>
               <LinkContainer to="/">
                 <Navbar.Brand>Liquor Store</Navbar.Brand>
               </LinkContainer>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
+                <SearchBox />
                 <Nav className="me-auto  w-100  justify-content-end">
                   <Link to="/cart" className="nav-link">
-                    Cart
+                    Carrito
                     {cart.cartItems.length > 0 && (
                       <Badge pill bg="danger">
                         {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
@@ -53,10 +78,10 @@ function App() {
                   {userInfo ? (
                     <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
                       <LinkContainer to="/profile">
-                        <NavDropdown.Item>User Profile</NavDropdown.Item>
+                        <NavDropdown.Item>Mi Perfíl</NavDropdown.Item>
                       </LinkContainer>
                       <LinkContainer to="/orderhistory">
-                        <NavDropdown.Item>Order History</NavDropdown.Item>
+                        <NavDropdown.Item>Historial de Ordenes</NavDropdown.Item>
                       </LinkContainer>
                       <NavDropdown.Divider />
                       <Link
@@ -64,7 +89,7 @@ function App() {
                         to="#signout"
                         onClick={signoutHandler}
                       >
-                        Sign Out
+                        Cerrar Sesión
                       </Link>
                     </NavDropdown>
                   ) : (
@@ -77,6 +102,27 @@ function App() {
             </Container>
           </Navbar>
         </header>
+        <div className={
+          sidebarIsOpen
+            ? 'active-nav sidebar-navbar d-flex justify-content-between flex-wrap flex-column'
+            : 'sidebar-navbar d-flex justify-content-between flex-wrap flex-column'
+        }>
+          <Nav className="flex-column text-white w-100 p-2">
+            <Nav.Item>
+              <strong>Categorias</strong>
+            </Nav.Item>
+            {categories.map((category) => (
+              <Nav.Item key={category}>
+                <LinkContainer
+                  to={`/search?category=${category}`}
+                  onClick={() => setSidebarIsOpen(false)}
+                >
+                  <Nav.Link>{category}</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </div>
 
         <main>
           <Container className='mt-3'>
@@ -90,7 +136,8 @@ function App() {
               <Route path="/payment" element={<PaymentMethodScreen />} />
               <Route path="/placeorder" element={<PlaceOrderScreen />} />
               <Route path="/order/:id" element={<OrderScreen />} />
-              <Route path="/orderhistory" element={<OrderHistoryScreen />}></Route>
+              <Route path="/orderhistory" element={<OrderHistoryScreen />} />
+              <Route path="/profile" element={<ProfileScreen />} />
             </Routes>
           </Container>
         </main>
@@ -98,7 +145,7 @@ function App() {
           <div className='text-center'>All Right Reserved</div>
         </footer>
       </div>
-    </BrowserRouter>
+    </BrowserRouter >
   );
 }
 
